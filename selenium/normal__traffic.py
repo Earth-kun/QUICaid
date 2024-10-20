fa0a4ce252328f0085f392788ec09a822c23dbed
rm <jqmundo@up.edu.ph>
2024-10-21 00:16:03 +0800

    added ig script

---
+++ selenium/normal__traffic.py
@@ -0,0 +1,42 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+options = webdriver.ChromeOptions()
+#options.add_argument('--headless')
+#options.add_argument('--no-sandbox')
+#options.add_argument('--disable-dev-shm-usage')
+service = Service(executable_path="./chromedriver")
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get('https://www.youtube.com')
+
+WebDriverWait(driver, 5).until (
+	EC.presence_of_element_located((By.NAME, 'search_query'))
+)
+
+input_element = driver.find_element(By.NAME, 'search_query')
+input_element.send_keys("QUIC")
+time.sleep(1)
+input_element.send_keys(Keys.ENTER)
+
+WebDriverWait(driver, 5).until (
+	EC.presence_of_element_located((By.ID, 'video-title'))
+)
+vid = driver.find_element(By.ID, 'video-title')
+vid.click()
+
+WebDriverWait(driver, 10).until (
+	EC.presence_of_element_located((By.CLASS_NAME, 'ytp-skip-ad-button'))
+)
+time.sleep(5)
+ad1 = driver.find_element(By.CLASS_NAME, 'ytp-skip-ad-button__icon')
+ad1.click()
+
+time.sleep(60)
+driver.quit()
