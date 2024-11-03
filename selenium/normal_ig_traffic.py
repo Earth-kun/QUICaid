251db3e6d78f41270e054b4633fa9c282d018316
Earth-kun <j.raphaelmundo@gmail.com>
2024-11-03 22:21:16 +0800

    add ig pcapng and script

--- selenium/normal_ig_traffic.py
+++ selenium/normal_ig_traffic.py
@@ -6,45 +6,39 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 options = webdriver.ChromeOptions()
 #options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+#options.add_argument('--no-sandbox')
+#options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-notifications')
 options.add_argument('--start-maximized')
-options.add_argument('--disbale-infobars')
+#options.add_argument('--disbale-infobars')
 service = Service(executable_path="./chromedriver")
 driver = webdriver.Chrome(service=service, options=options)
 
 #JavascriptExecutor js = (JavascriptExecutor) driver;
 
-driver.get('https://www.facebook.com')
+driver.get('https://www.instagram.com')
 
 WebDriverWait(driver, 5).until (
-	EC.presence_of_element_located((By.NAME, 'email'))
+	EC.presence_of_element_located((By.NAME, 'username'))
 )
-input_email = driver.find_element(By.NAME, 'email')
+input_email = driver.find_element(By.NAME, 'username')
 input_email.send_keys("sslnetworks1@gmail.com")
 time.sleep(1)
-input_pass = driver.find_element(By.NAME, 'pass')
+input_pass = driver.find_element(By.NAME, 'password')
 input_pass.send_keys("ilovessl")
 time.sleep(1)
 input_pass.send_keys(Keys.ENTER)
 
 WebDriverWait(driver, 5).until (
-	EC.presence_of_element_located((By.ID, ':r1e:'))
+	EC.presence_of_element_located((By.CSS_SELECTOR, "[aria-label='Reels']"))
 )
-videos = driver.find_element(By.ID, ':r1e:')
-videos.click()
+reels = driver.find_element(By.CSS_SELECTOR, "[aria-label='Reels']")
+reels.click()
 
-#driver.execute_script("window.scrollTo(0,70)")
-
-WebDriverWait(driver, 5).until (
-	EC.presence_of_element_located((By.ID, 'watch_feed'))
-)
-vid1 = driver.find_element(By.CLASS_NAME, 'x1n2onr6')
-vid1.click()
 
 time.sleep(60)
 driver.quit()
