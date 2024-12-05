b2b280a696a70077cf49ee80ad24e24f081746db
Earth-kun <jqmundo@up.edu.ph>
2024-12-06 02:06:31 +0800

    update script

--- Data Collection/benign.sh
+++ Data Collection/benign.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #capture UPD packets (inlcuding QUIC) at port 443 for 5 minutes
-#fields: SRC_IP, DST_IP, SRC_PORT, DST_PORT, QUIC_VERSION, BYTES, PROTOCOL
+#fields: DURATION, SRC_IP, DST_IP, SRC_PORT, DST_PORT, QUIC_VERSION, BYTES, PROTOCOL
 tshark -f "udp port 443" -i ens18 -a duration:300 -t r -T fields \
--e ip.src -e ip.dst -e ip.geoip.asnum -e udp.srcport -e udp.dstport -e quic.version -e frame.len -e _ws.col.Protocol \
+-e frame.time -e ip.src -e ip.dst -e udp.srcport -e udp.dstport -e quic.version -e udp.payload -e _ws.col.Protocol \
 -E separator=, -E quote=d > ./benign_flow/test1.csv
+
+python3 benign_yt_traffic.py
