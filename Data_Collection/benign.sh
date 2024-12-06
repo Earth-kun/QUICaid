b5be00c8e07000fc1706b7f145e72ed5ab8f52d2
Earth-kun <jqmundo@up.edu.ph>
2024-12-06 13:34:06 +0800

    upload test1.csv

--- Data_Collection/benign.sh
+++ Data_Collection/benign.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+read -p "enter test #:" flow
 commands=(
 	"python3 benign_yt_traffic.py"	
 	"tshark -f 'udp port 443' -i ens18 -a duration:300 -t r -T fields \
-	-e frame.time -e ip.src -e ip.dst -e udp.srcport -e udp.dstport -e quic.version \
+	-e frame.time_relative -e ip.src -e ip.dst -e udp.srcport -e udp.dstport -e quic.version \
 	-e quic.packet_length -e _ws.col.Protocol \
-	-E separator=, -E quote=d > ./benign_flow/test1.csv"
+	-E separator=, -E quote=d > ./benign_flow/test$flow.csv"
 )
 
 parallel --delay 10 ::: "${commands[@]}"
