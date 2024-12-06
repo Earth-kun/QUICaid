8d25483971c56cb82da19bca170433b30fe22984
Earth-kun <jqmundo@up.edu.ph>
2024-12-06 13:15:23 +0800

    rewrite test1.csv

--- Data_Collection/benign.sh
+++ Data_Collection/benign.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 commands=(
+	"python3 benign_yt_traffic.py"	
 	"tshark -f 'udp port 443' -i ens18 -a duration:300 -t r -T fields \
-	-e frame.time -e ip.src -e ip.dst -e udp.srcport -e udp.dstport -e quic.version -e udp.payload -e _ws.col.Protocol \
+	-e frame.time -e ip.src -e ip.dst -e udp.srcport -e udp.dstport -e quic.version \
+	-e quic.packet_length -e _ws.col.Protocol \
 	-E separator=, -E quote=d > ./benign_flow/test1.csv"
-	"python3 benign_yt_traffic.py"
 )
 
 parallel --delay 10 ::: "${commands[@]}"
