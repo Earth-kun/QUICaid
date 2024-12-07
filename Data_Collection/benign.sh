61ba725c0018c6564f608584d7ec6a74cdd3578c
Earth-kun <jqmundo@up.edu.ph>
2024-12-07 17:09:49 +0800

    upload 30 benign ig tests

--- Data_Collection/benign.sh
+++ Data_Collection/benign.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-command1="python3 benign_yt_traffic.py"
+command1="python3 benign_igreels_traffic.py"
 
-for i in {21..30}; do
+for i in {1..30}; do
 	parallel --delay 3 ::: "$command1" "tshark -f 'udp port 443' -i ens18 -a duration:300 -t r -T fields \
 	-e frame.time_relative -e ip.src -e ip.dst -e udp.srcport -e udp.dstport -e quic.version \
 	-e quic.packet_length -e _ws.col.Protocol \
-	-E separator=, -E quote=d > ./benign_flow/test$i.csv"
+	-E separator=, -E quote=d > ./benign_flow/ig_test/test$i.csv"
 done
 
 
