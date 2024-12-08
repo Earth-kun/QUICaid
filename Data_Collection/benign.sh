b51231598e56b9dcb3d4f7d819985ed54fea6282
Earth-kun <jqmundo@up.edu.ph>
2024-12-08 15:09:05 +0800

    upload ig and fb tests

--- Data_Collection/benign.sh
+++ Data_Collection/benign.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-command1="python3 benign_igreels_traffic.py"
+command1="python3 benign_fbvids_traffic.py"
 
 for i in {1..30}; do
 	parallel --delay 3 ::: "$command1" "tshark -f 'udp port 443' -i ens18 -a duration:300 -t r -T fields \
 	-e frame.time_relative -e ip.src -e ip.dst -e udp.srcport -e udp.dstport -e quic.version \
 	-e quic.packet_length -e _ws.col.Protocol \
-	-E separator=, -E quote=d > ./benign_flow/ig_test/test$i.csv"
+	-E separator=, -E quote=d > ./benign_flow/fb_test/test$i.csv"
 done
 
 
