a828ddef0d8efda1704e6af7cad9b767b0c5a508
almncarlo <cmalamani@up.edu.ph>
2025-02-10 01:00:26 +0800

    added support for oneclasssvm model

--- tuning/prequential.py
+++ tuning/prequential.py
@@ -1,5 +1,6 @@
 import numpy as np
 import copy
+import skmultiflow
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 import time
@@ -8,13 +9,7 @@ from tracemalloc import Snapshot
 
 
 def run_prequential(classifier, stream, feature_selector, n_pretrain=200):
-    """
-    Parameters
-    ----------
-    setup (str): 
-    classifier
-    etc...
-    """
+
     stream.restart()
     n_samples, correct_predictions = 0, 0
     true_labels, pred_labels = [], []
@@ -23,8 +18,13 @@ def run_prequential(classifier, stream, feature_selector, n_pretrain=200):
     # print(f"Evaluating {setup_name} configuration.")
 
     # pretrain samples
-    X_pretrain, y_pretrain = stream.next_sample(n_pretrain)
-    classifier.partial_fit(X_pretrain, y_pretrain, classes=stream.target_values)
+    for _ in range(n_pretrain):
+        X_pretrain, y_pretrain = stream.next_sample()
+        # print(dict(enumerate(*X_pretrain)))
+        if isinstance(classifier, skmultiflow.meta.AdaptiveRandomForestClassifier) or isinstance(classifier, skmultiflow.lazy.KNNClassifier) or isinstance(classifier, skmultiflow.lazy.KNNADWINClassifier):
+            classifier.partial_fit(X_pretrain, y_pretrain, classes=stream.target_values)
+        else:
+            classifier.learn_one(dict(enumerate(*X_pretrain)))
     
     # print(f"Model pretrained on {n_pretrain} samples.")
 
@@ -37,23 +37,41 @@ def run_prequential(classifier, stream, feature_selector, n_pretrain=200):
             # with dynamic feature selection
             feature_selector.weight_features(copy.copy(X), copy.copy(y))
             X_select = feature_selector.select_features(copy.copy(X), rng=np.random.default_rng())
-            y_pred = classifier.predict(X_select)
+            # y_pred = classifier.predict(X_select)
+            if isinstance(classifier, skmultiflow.meta.AdaptiveRandomForestClassifier) or isinstance(classifier, skmultiflow.lazy.KNNClassifier) or isinstance(classifier, skmultiflow.lazy.KNNADWINClassifier):
+                y_pred = classifier.predict(X_select)
+            else:
+                score = classifier.score_one(dict(enumerate(*X_select)))
+                y_pred = classifier.classify(score)
             
             # Train incrementally
-            classifier.partial_fit(copy.copy(X_select), [y[0]])
+            if isinstance(classifier, skmultiflow.meta.AdaptiveRandomForestClassifier) or isinstance(classifier, skmultiflow.lazy.KNNClassifier) or isinstance(classifier, skmultiflow.lazy.KNNADWINClassifier):
+                classifier.partial_fit(copy.copy(X_select), [y[0]])
+            else:
+                classifier.learn_one(copy.copy(dict(enumerate(*X_select))))
 
         else:
             # no feature selection
-            y_pred = classifier.predict(X)
+            if isinstance(classifier, skmultiflow.meta.AdaptiveRandomForestClassifier) or isinstance(classifier, skmultiflow.lazy.KNNClassifier) or isinstance(classifier, skmultiflow.lazy.KNNADWINClassifier):
+                y_pred = classifier.predict(X)
+            else:
+                score = classifier.score_one(dict(enumerate(*X)))
+                y_pred = classifier.classify(score)
             
             # Train incrementally
-            classifier.partial_fit(copy.copy(X), [y[0]])
+            if isinstance(classifier, skmultiflow.meta.AdaptiveRandomForestClassifier) or isinstance(classifier, skmultiflow.lazy.KNNClassifier) or isinstance(classifier, skmultiflow.lazy.KNNADWINClassifier):
+                classifier.partial_fit(copy.copy(X), [y[0]])
+            else:
+                classifier.learn_one(copy.copy(dict(enumerate(*X))))
         
         if y_pred == y:
             correct_predictions += 1
         
         true_labels.append(y[0])
-        pred_labels.append(y_pred[0])
+        if isinstance(classifier, skmultiflow.meta.AdaptiveRandomForestClassifier) or isinstance(classifier, skmultiflow.lazy.KNNClassifier) or isinstance(classifier, skmultiflow.lazy.KNNADWINClassifier):
+            pred_labels.append(y_pred[0])
+        else:
+            pred_labels.append(y_pred)
 
         # check for drift
         # if drift_detector is not None:
