409d8522bb7c5936fba5b4ada0823a5aceb1abf4
Earth-kun <jqmundo@up.edu.ph>
2025-03-30 13:25:48 +0800

    upload 2nd test

--- live_program/test.sh
+++ live_program/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-parser="~/QUIC/live_program/quicaid.sh 1 0 10.10.3.10 out1.csv"
+parser="~/QUIC/live_program/quicaid.sh 1 0 10.10.3.10 out2.csv"
 
 parallel ::: "$parser" "~/QUIC/Data_Collection/benign_new.sh"
